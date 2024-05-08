a951284367710706c0151d284fe8b05a97fe86dc
RocioMartinez98 <rocioabigailmartinez98@gmail.com>
2024-05-08 19:52:14 -0300

    Primera iteración

---
+++ src/main/java/models/Usuario.java
@@ -0,0 +1,61 @@
+package models;
+
+import jakarta.persistence.*;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Entity
+@Table(name="usuario")
+public class Usuario {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private long id;
+    private String correo;
+    private String clave;
+
+    @OneToMany
+    @JoinTable(name="notasDeUsuario", joinColumns = {@JoinColumn(name = "usuario_id")},inverseJoinColumns = {@JoinColumn(name="nota_id")})
+    private List<Nota> notasDeUsuario = new ArrayList<>();
+
+    public Usuario(){}
+
+    public Usuario(long id, String correo, String clave) {
+        this.id = id;
+        this.correo = correo;
+        this.clave = clave;
+    }
+
+    public List<Nota> getNotasDeUsuario(){
+        return notasDeUsuario;
+    }
+
+    public void addNotasDeUsuario(Nota notaDeUsuario){
+        notasDeUsuario.add(notaDeUsuario);
+    }
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public String getCorreo() {
+        return correo;
+    }
+
+    public void setCorreo(String correo) {
+        this.correo = correo;
+    }
+
+    public String getClave() {
+        return clave;
+    }
+
+    public void setClave(String clave) {
+        this.clave = clave;
+    }
+}
+
---
+++ src/main/resources/schema.sql
@@ -0,0 +1,20 @@
+CREATE TABLE nota{
+    id BIGSERIAL PRIMARY KEY,
+    titulo VARCHAR(255) NOT NULL,
+    tipo INT NOT NULL,
+    texto VARCHAR(255)
+}
+
+CREATE TABLE usuario{
+    id BIGSERIAL PRIMARY KEY,
+    clave VARCHAR(255) NOT NULL,
+    correo VARCHAR(255) NOT NULL
+}
+
+CREATE TABLE notasDeUsuario{
+    usuario_id BIGSERIAL,
+    nota_id BIGSERIAL,
+    PRIMARY KEY(usuario_id, nota_id),
+    FOREIGN KEY (usuario_id) REFERENCES usuario(id),
+    FOREIGN KEY (nota_id) REFERENCES nota(id)
+}
